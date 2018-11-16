ddd4dfc0f47e095acc1001302f81dad296b54191
Timothy Dillman <tdillman@taxslayer.com>
2018-11-16 11:39:35 -0500

    update sample

--- examples/Application.kt
+++ examples/Application.kt
@@ -5,6 +5,11 @@ class SampleApp: Application() {
     override fun onCreate() {
         super.onCreate()
         TSAnalyticsManager.addPackage(TSAppsee("123SampleAPIKEY", mutableListOf(TSPIILevel.NOT_SENSITIVE), null))
-
+        // create an item to track with a PII level
+        val point = TSTrackableItem(key = "sampleItem", value = "someValue" as Any, level = TSPIILevel.NOT_SENSITIVE)
+        // create a trackable item with a set of values to track as an event
+        val trackable = TSTrackable(eventName = "sampleEventName", values = mutableListOf(point))
+        // pass the trackable item to the package manager and let all the packages know an event needs to be recorded
+        TSAnalyticsManager.track(trackable)
     }
 }
